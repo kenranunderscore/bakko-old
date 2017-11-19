64856d63c7dc5a55b74e9f0fb660b67e573b0d8d
kenranunderscore <johb.maier@gmail.com>
2017-11-19 20:46:05 +0100

    Add radar
    Adds a simple 1v1 locking radar to Samael.

--- Samael/Samael.cs
+++ Samael/Samael.cs
@@ -1,14 +1,23 @@
 ﻿namespace kenran.Samael
 {
     using Robocode;
+    using Robocode.Util;
 
     public class Samael : AdvancedRobot
     {
         public override void Run()
         {
+            SetTurnRadarRightRadians(double.MaxValue);
+
             while (true)
             {
+                Execute();
             }
+        }
+
+        public override void OnScannedRobot(ScannedRobotEvent evnt)
+        {
+            SetTurnRadarRightRadians(2.0d * Utils.NormalRelativeAngle(HeadingRadians + evnt.BearingRadians - RadarHeadingRadians));
         }
     }
 }
