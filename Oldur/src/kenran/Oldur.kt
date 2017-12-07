d4122467c7af6623d3211659866c98b257d128a8
kenranunderscore <johb.maier@gmail.com>
2017-12-07 22:04:56 +0100

    Add utilities
    Adds the Utils file containing several utility methods.

--- Oldur/src/kenran/Oldur.kt
+++ Oldur/src/kenran/Oldur.kt
@@ -1,19 +1,26 @@
 package kenran
 
+import kenran.movement.Surfboard
 import robocode.AdvancedRobot
 import kenran.radar.LockingRadar
 import robocode.ScannedRobotEvent
 
-const val RADAR_LOCK_MULTIPLIER: Double = 2.0
-
 class Oldur : AdvancedRobot() {
-	private val _radar: LockingRadar
+    companion object {
+        private const val RADAR_LOCK_MULTIPLIER: Double = 2.0
+    }
 
-	init {
-		_radar = LockingRadar(this, RADAR_LOCK_MULTIPLIER)
-	}
+    private val _radar: LockingRadar
+    private var _surfboard: Surfboard? = null
+
+    init {
+        _radar = LockingRadar(this, RADAR_LOCK_MULTIPLIER)
+    }
+
 
 	override fun run() {
+        _surfboard = Surfboard(this)
+
 		isAdjustGunForRobotTurn = true
 		isAdjustRadarForGunTurn = true
 
--- Oldur/src/kenran/movement/Surfboard.kt
+++ Oldur/src/kenran/movement/Surfboard.kt
@@ -12,7 +12,7 @@ class Surfboard(bot: AdvancedRobot) {
         private var _surfStats: DoubleArray = DoubleArray(BINS)
     }
     
-    private val _bot: AdvancedRobot
+    private val _bot: AdvancedRobot = bot
     private var _enemyEnergy: Double = 100.0
     private var _position: Point2D.Double
     private var _enemyPosition: Point2D.Double
@@ -21,16 +21,14 @@ class Surfboard(bot: AdvancedRobot) {
     private var _absoluteBearings = listOf<Double>()
     
     init {
-        _bot = bot
         _position = Point2D.Double(_bot.getX(), _bot.getY())
         _enemyPosition = Point2D.Double()
         _fieldRect = Rectangle2D.Double(18.0, 18.0, 800.0, 600.0)
     }
     
     fun onScannedRobot(e: ScannedRobotEvent) {
-        _position = Point2D.Double(_bot.getX(), _bot.getY())
-        val lateralVelocity = _bot.getVelocity() * Math.sin(e.getBearingRadians())
-        val absoluteBearing = e.getBearingRadians() + _bot.getHeadingRadians()
-        //_surfDirections.
+        _position = Point2D.Double(_bot.x, _bot.y)
+        val lateralVelocity = _bot.velocity * Math.sin(e.bearingRadians)
+        val absoluteBearing = e.bearingRadians + _bot.headingRadians
     }
 }
